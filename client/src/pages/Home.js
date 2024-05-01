66143117ebde67d02a29d7f22ce775af51150094
DESKTOP-JOLHK00\DELL <21520890@gm.uit.edu.vn>
2024-05-01 22:16:39 +0700

    addListToMainPage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,35 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 function Home() {
-  const [listOfPosts,setListOfPosts] = useState([]);
-  let navigate = useNavigate();
-  //logic GET request render
+  const [listOfProducts, setListOfProducts] = useState([]);
+  const navigate = useNavigate();
+
+  // Fetch products data
   useEffect(() => {
-    axios.get("http://localhost:2001/posts").then((response) => {
-      setListOfPosts(response.data);
-    });
+    axios
+      .get("http://localhost:2001/products")
+      .then((response) => {
+        setListOfProducts(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching products:", error);
+      });
   }, []);
 
   return (
     <div>
-      <div className="listOfProd">
-        {listOfPosts.map((value, key) => {
-          return (
-            <div className='post' onClick={() => {navigate(`/Post/${value.id}`)}} >
-              <br></br>
-              <div className='title'> {value.title}</div>
-              <div className='body'> {value.postText}</div>
-              <div className='footer'> {value.username}</div>
-            </div>
-          );
-        })}
+      <div className="listOfProducts">
+        {listOfProducts.map((value, key) => (
+          <div
+            className="product"
+            key={key}
+            onClick={() => navigate(`/Product/${value.id}`)}
+          >
+            <br />
+            <div className="name"> {value.Name}</div>
+            <div className="type"> {value.Type}</div>
+            {value.ModRank !== null && (
+              <div className="modrank"> Mod Rank: {value.ModRank}</div>
+            )}
+            {value.Rarity && (
+              <div className="rarity"> Rarity: {value.Rarity}</div>
+            )}
+            {value.Duncat && (
+              <div className="duncat"> Duncat: {value.Duncat}</div>
+            )}
+            {value.MasteryRank && (
+              <div className="masteryrank">
+                {" "}
+                Mastery Rank: {value.MasteryRank}
+              </div>
+            )}
+            <div className="tax"> Tax: {value.Tax}</div>
+          </div>
+        ))}
       </div>
-      
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
--- client/src/pages/home-page/HomePage.js
+++ client/src/pages/home-page/HomePage.js
@@ -1,73 +1,86 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from "react";
 // import { useParams }  from 'react-router-dom'
-import axios from 'axios'
-import '../home-page/HomePage.css'
-import '../search-section/SearchSection.css';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import './HomePage.css';
-import warframeImg from '../../assets/gauss.png'
-import weaponImg from '../../assets/AcceltraPrime.webp'
-import modImg from '../../assets/mod.webp'
-import midenlacdit from '../../assets/platinum.webp'
+import axios from "axios";
+import "../home-page/HomePage.css";
+import "../search-section/SearchSection.css";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import "./HomePage.css";
+import warframeImg from "../../assets/gauss.png";
+import weaponImg from "../../assets/AcceltraPrime.webp";
+import modImg from "../../assets/mod.webp";
+import midenlacdit from "../../assets/platinum.webp";
 
 function HomePage() {
-    // let { id } = useParams();
-    const [postObject, setPostObject] = useState([])
-    useEffect(()=>{
-        axios.get(`http://localhost:2001/posts`).then((response) => {
-            setPostObject(response.data)
-        });
-    }, [/*id*/])
+  // let { id } = useParams();
+  const [postObject, setPostObject] = useState([]);
+  useEffect(
+    () => {
+      axios.get(`http://localhost:2001/products`).then((response) => {
+        setPostObject(response.data);
+      });
+    },
+    [
+      /*id*/
+    ]
+  );
   return (
-      <div className="homepage">
-            <div className="homeContainer">
-                <div className='searchCategory'>
-                    <Link to="/Warframe" className='searchbutt'>
-                        <img src={warframeImg} className='img'></img>
-                        Warframe
-                    </Link>
-                </div>
-                
-                <div className='searchCategory'>
-                    <Link to="/Weapons" className='searchbutt'>
-                        <img src={weaponImg} className='img'></img>
-                        Weapons
-                    </Link>
-                </div>
-                
-                <div className='searchCategory'>
-                    <Link to="/Mod" className='searchbutt'>
-                        <img src={modImg} className='img'></img>
-                        Mods
-                    </Link>
-                </div>
-                
-                <div className='searchCategory'>
-                    <Link to="/PlaceOrder" className='searchbutt'>
-                        <img src={midenlacdit} className='img'></img>
-                        search
-                    </Link>
-                </div>
-            </div>
-
-            <div className="productList">
-                {postObject.map((value,key)=> (
-                    <div className="productCapsule">
-                        <div className="title">{value.title}</div>
-                        <div className="postText">{value.postText}</div>
-                        <div className="username">{value.username}</div>
-                        {/* <div className="title">title</div>
-                        <div className="postText">quantity</div>
-                        <div className="username">price</div> */}
-                        <br></br>
-                    </div>
-                ))}
-                
-                
-                
-            </div>
+    <div className="homepage">
+      <div className="homeContainer">
+        <div className="searchCategory">
+          <Link to="/Warframe" className="searchbutt">
+            <img src={warframeImg} className="img"></img>
+            Warframe
+          </Link>
         </div>
-  )
+
+        <div className="searchCategory">
+          <Link to="/Weapons" className="searchbutt">
+            <img src={weaponImg} className="img"></img>
+            Weapons
+          </Link>
+        </div>
+
+        <div className="searchCategory">
+          <Link to="/Mod" className="searchbutt">
+            <img src={modImg} className="img"></img>
+            Mods
+          </Link>
+        </div>
+
+        <div className="searchCategory">
+          <Link to="/PlaceOrder" className="searchbutt">
+            <img src={midenlacdit} className="img"></img>
+            search
+          </Link>
+        </div>
+      </div>
+
+      <div className="productList">
+        {postObject.map((value, key) => (
+          <div className="productCapsule">
+            <div className="name"> {value.Name}</div>
+            <div className="type"> {value.Type}</div>
+            {value.ModRank !== null && (
+              <div className="modrank"> Mod Rank: {value.ModRank}</div>
+            )}
+            {value.Rarity && (
+              <div className="rarity"> Rarity: {value.Rarity}</div>
+            )}
+            {value.Duncat && (
+              <div className="duncat"> Duncat: {value.Duncat}</div>
+            )}
+            {value.MasteryRank && (
+              <div className="masteryrank">
+                {" "}
+                Mastery Rank: {value.MasteryRank}
+              </div>
+            )}
+            <div className="tax"> Tax: {value.Tax}</div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default HomePage
+export default HomePage;
